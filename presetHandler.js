ce013628721fffc9db6a156d502db4b984932c34
Chelsea Castelli <chel.castel@aol.com>
2022-05-07 14:04:10 -0400

    complete all non-bonus tasks and pass tests

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,26 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (request, index, newPresetArray=null) => {
+    let returnArr = [];
+    // if index invalid -- 404 Not Found, otherwise, 200 OK
+    if (index < 0 || index >= presets.length) returnArr.push(404)
+    else returnArr.push(200)
+
+    if (request === 'GET') returnArr.push(presets[index])
+
+    else if (request === 'PUT') {
+        presets[index] = newPresetArray;
+        returnArr.push(presets[index]);
+    } 
+
+    // if request invalid -- 400 Bad Request as first element
+    else returnArr.unshift(400)
+
+    
+    return returnArr;
+
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,35 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = Array(16).fill(false);
+let snares = Array(16).fill(false);
+let hiHats = Array(16).fill(false);
+let rideCymbals = Array(16).fill(false);
+
+let drumKit = {
+    'kicks': kicks,
+    'snares': snares,
+    'hiHats': hiHats,
+    'rideCymbals': rideCymbals
+}
+
+const toggleDrum = (drumPad, index) => {
+    // check for missing arguments or invalid array name
+    if ((drumPad === undefined || index === undefined) || (!drumKit[drumPad])) return;
+
+    if (index >= drumKit[drumPad].length || index < 0) return;
+
+    drumKit[drumPad][index] = !drumKit[drumPad][index];
+}
+
+const clear = drumPad => {
+    if ((drumPad === undefined) || (!drumKit[drumPad])) return;
+
+    for (let i = 0; i < drumKit[drumPad].length; i++) { drumKit[drumPad][i] = false; }
+}
+
+const invert = drumPad => {
+    if ((drumPad === undefined) || (!drumKit[drumPad])) return;
+
+    for (let i = 0; i < eval(drumPad).length; i++) { eval(drumPad)[i] = !eval(drumPad)[i]; }
+}
+
 
